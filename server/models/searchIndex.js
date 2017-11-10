12883c57632515ee224852e70534137f59bfae26
arjunkashyap <arjun.kashyap@yahoo.co.in>
2017-11-10 20:28:03 +0530

    index now working

--- server/models/searchIndex.js
+++ server/models/searchIndex.js
@@ -4,7 +4,7 @@ module.exports = function(volume){
 	const fs = require('fs');
 	const path = require('path');
 
-	var indexData = JSON.parse(fs.readFileSync(path.join(__dirname, '../public/index/searchIndex' + volume + '.json'), 'utf8'));
+	var indexData = JSON.parse(fs.readFileSync(path.join(__dirname, '../../public/index/searchIndex' + volume + '.json'), 'utf8'));
 	var searchIndex = elasticlunr.Index.load(indexData)
 
 	return searchIndex;
--- server/routes/createIndex.js
+++ server/routes/createIndex.js
@@ -9,31 +9,31 @@ router.get('/', function(req, res){
 
 	var pad = "000";
     
-	var indexData = JSON.parse(fs.readFileSync(path.join(__dirname, '../public/index/searchIndex001.json'), 'utf8'));
-	var searchIndex = elasticlunr.Index.load(indexData);
+	// var indexData = JSON.parse(fs.readFileSync(path.join(__dirname, '../../public/index/searchIndex001.json'), 'utf8'));
+	// var searchIndex = elasticlunr.Index.load(indexData);
 
-    for(i=2;i<=61;i++) {
+    for(i=1;i<=61;i++) {
 
-    	// var searchIndex = elasticlunr(function () {
+    	var searchIndex = elasticlunr(function () {
 
-    	//     this.addField('title');
-    	//     this.addField('authorname');
-    	//     this.addField('text');
-    	//     this.setRef('titleid');
-    	//     this.saveDocument(false);
-    	// });
+    	    this.addField('title');
+    	    this.addField('authorname');
+    	    this.addField('text');
+    	    this.setRef('titleid');
+    	    this.saveDocument(false);
+    	});
 
 		var str = "" + i;
 		var id = pad.substring(0, pad.length - str.length) + str;
 
-		var volumeData = JSON.parse(fs.readFileSync(path.join(__dirname, '../source/' + id + '.json'), 'utf8'))
+		var volumeData = JSON.parse(fs.readFileSync(path.join(__dirname, '../../public/source/' + id + '.json'), 'utf8'))
 		for(j=0;j<volumeData.length;j++) {
 
 			console.log(volumeData[j]['titleid']);
 	        searchIndex.addDoc(volumeData[j]);
 	    }
 	
-		fs.writeFileSync(path.join(__dirname, '../public/index/searchIndex' + id + '.json'), JSON.stringify(searchIndex));
+		fs.writeFileSync(path.join(__dirname, '../../public/index/searchIndex' + id + '.json'), JSON.stringify(searchIndex));
 	}
 
 });
