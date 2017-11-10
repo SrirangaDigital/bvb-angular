48126fc6ef5b3d7c69ebf66c0cd5af89963c890d
arjunkashyap <arjun.kashyap@yahoo.co.in>
2017-11-10 20:07:52 +0530

    MEAN init

---
+++ server.js
@@ -0,0 +1,44 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const path = require('path');
+const http = require('http');
+
+// Init app
+const app = express();
+
+// Bring in Models
+let Article = require('./server/models/article');
+
+// Body parser Middleware
+// parse application/json
+app.use(bodyParser.json());
+// parse application/x-www-form-urlencoded
+app.use(bodyParser.urlencoded({ extended: false }));
+
+// Angular DIST output folder
+app.use(express.static(path.join(__dirname, 'dist')));
+
+// Set Public folder
+app.use(express.static(path.join(__dirname, 'public')));
+
+
+// Route Files
+const api = require('./server/routes/api');
+app.use('/api', api);
+
+const createIndex = require('./server/routes/createIndex');
+app.use('/createIndex', createIndex);
+
+// Send all other requests to the Angular app
+app.get('*', (req, res) => {
+    res.sendFile(path.join(__dirname, 'dist/index.html'));
+});
+
+
+//Set Port
+const port = process.env.PORT || '3000';
+app.set('port', port);
+
+const server = http.createServer(app);
+
+server.listen(port, () => console.log(`Running on localhost:${port}`));
---
+++ server/routes/createIndex.js
@@ -0,0 +1,41 @@
+const express = require('express');
+const router = express.Router();
+const elasticlunr = require('elasticlunr');
+const fs = require('fs');
+const path = require('path');
+
+// Create index and store it to a file
+router.get('/', function(req, res){
+
+	var pad = "000";
+    
+	var indexData = JSON.parse(fs.readFileSync(path.join(__dirname, '../public/index/searchIndex001.json'), 'utf8'));
+	var searchIndex = elasticlunr.Index.load(indexData);
+
+    for(i=2;i<=61;i++) {
+
+    	// var searchIndex = elasticlunr(function () {
+
+    	//     this.addField('title');
+    	//     this.addField('authorname');
+    	//     this.addField('text');
+    	//     this.setRef('titleid');
+    	//     this.saveDocument(false);
+    	// });
+
+		var str = "" + i;
+		var id = pad.substring(0, pad.length - str.length) + str;
+
+		var volumeData = JSON.parse(fs.readFileSync(path.join(__dirname, '../source/' + id + '.json'), 'utf8'))
+		for(j=0;j<volumeData.length;j++) {
+
+			console.log(volumeData[j]['titleid']);
+	        searchIndex.addDoc(volumeData[j]);
+	    }
+	
+		fs.writeFileSync(path.join(__dirname, '../public/index/searchIndex' + id + '.json'), JSON.stringify(searchIndex));
+	}
+
+});
+
+module.exports = router;
