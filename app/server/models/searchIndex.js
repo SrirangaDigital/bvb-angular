d95955b260a8191bbc39be20fba3c6a207c0abe6
arjunkashyap <arjun.kashyap@yahoo.co.in>
2018-03-05 21:24:45 +0530

    indewx.asar - trial

--- app/server/models/searchIndex.js
+++ app/server/models/searchIndex.js
@@ -3,12 +3,8 @@ module.exports = function(volume){
 	const elasticlunr = require('elasticlunr');
 	const fs = require('fs');
 	const path = require('path');
-	var os = require('os');
 
-	var commonPath = require('../models/commonPath');
-	if(!(fs.existsSync(commonPath))) return '';
-
-	var indexData = JSON.parse(fs.readFileSync(commonPath + '/index/searchIndex' + volume + '.json', 'utf8'));
+	var indexData = JSON.parse(fs.readFileSync(path.join(__dirname, '../../public/index.asar/searchIndex' + volume + '.json', 'utf8'));
 	var searchIndex = elasticlunr.Index.load(indexData)
 
 	return searchIndex;
